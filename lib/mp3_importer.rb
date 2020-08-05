dbdee879e9f2ec8db1337e562488e963b96a4b42
Sarah Rogers <s.rogers0715@yahoo.com>
2020-08-05 01:24:53 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,23 @@
+require "pry"
+class MP3Importer
+attr_accessor :path
+
+def initialize(path)
+  @path = path
+end
+
+
+def files
+  files = Dir.glob("#{@path}/*.mp3")
+  #binding.pry
+  files.collect do |song|
+  song.gsub!("#{@path}/","")
+end
+end
+
+
+def import
+Song.new_by_filename(some_filename)
+Song << some_filename.create 
+end
+end
