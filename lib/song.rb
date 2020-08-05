16476cb37f0f78ee4919ffdf7be7150b066465b4
Sarah Rogers <s.rogers0715@yahoo.com>
2020-08-05 01:59:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,8 +10,10 @@ class Song
     @@all
   end
   def self.new_by_filename(filename)
-    song = self.new
-    song.artist, song.title = filename.split(" - ")
+  def self.new_by_filename(file)
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
     song
   end
 end
